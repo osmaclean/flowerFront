d7cd76fed6421e7f9c9b974269ea5db10cabd242
Maclean <contatolucasmaclean@gmail.com>
2024-03-26 11:54:56 -0300

    feat: initial setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Flowers Every Day',
+  description:
+    'Project developed to show a flower every day with your information',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="pt-BR">
+      <body className={inter.className}>{children}</body>
+    </html>
+  )
+}
