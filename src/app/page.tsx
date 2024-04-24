61984b57d9ad11484939be5bf2023ca28ee533b6
Maclean <contatolucasmaclean@gmail.com>
2024-04-24 19:56:46 -0300

    feat: finished project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -58,36 +58,43 @@ export default function Home() {
 
   return (
     <>
-      <section className="px-4 py-8 w-full flex flex-col justify-center gap-6 items-center">
-        <h1 className="text-white w-full text-center text-sm font-medium">
-          Você merece flores todos os dias minha rainha 💛
-        </h1>
-        <main className="bg-white rounded w-full p-4 flex flex-col justify-center items-start gap-4">
-          <Image
-            alt="Flower Image"
-            aria-label="Flower Image"
-            src="/flower.png"
-            width={200}
-            height={148}
-            className="h-36 w-full rounded"
-          ></Image>
-          <div className="flex flex-col justify-center items-start gap-1">
-            <h5 className="font-extrabold text-xs">Nome</h5>
-            <p className="font-normal text-xs">{flowers?.name}</p>
-          </div>
-          <div className="flex flex-col justify-center items-start gap-1">
-            <h5 className="font-extrabold text-xs">Descrição</h5>
-            <p className="font-normal text-xs">{flowers?.description}</p>
-          </div>
-          <div className="flex flex-col justify-center items-start gap-1">
-            <h5 className="font-extrabold text-xs">Frase feita com amor</h5>
-            <p className="font-normal text-xs">{flowers?.phrase}</p>
-          </div>
-        </main>
-        <footer className="w-full bg-black bg-opacity-50 rounded p-2 text-white flex flex-col justify-center items-center gap-1 font-medium text-xs">
-          <p>De: Maclean ⚡</p>
-          <p>Para: A mulher da minha vida 👰💒</p>
-        </footer>
+      <section className="relative w-full flex flex-col justify-center gap-6 items-center">
+        <img
+          src={flowers?.img_path}
+          alt="Flower Image"
+          className="absolute min-h-[125vh] object-cover -z-10 opacity-50 inset-0"
+        />
+        <div className="px-4 py-6 sm-1:px-8 sm-2:px-12 sm-3:px-16 w-full flex flex-col justify-center gap-6 items-center">
+          <h1 className="text-white w-full text-center text-sm font-medium">
+            Você merece flores todos os dias minha rainha 💛
+          </h1>
+          <main className="bg-white rounded w-full p-4 flex flex-col justify-center items-start gap-4">
+            <img
+              alt="Flower Image"
+              aria-label="Flower Image"
+              src={flowers?.img_path}
+              width={200}
+              height={148}
+              className="h-36 w-full rounded"
+            />
+            <div className="flex flex-col justify-center items-start gap-1">
+              <h5 className="font-extrabold text-xs">Nome</h5>
+              <p className="font-normal text-xs">{flowers?.name}</p>
+            </div>
+            <div className="flex flex-col justify-center items-start gap-1">
+              <h5 className="font-extrabold text-xs">Descrição</h5>
+              <p className="font-normal text-xs">{flowers?.description}</p>
+            </div>
+            <div className="flex flex-col justify-center items-start gap-1">
+              <h5 className="font-extrabold text-xs">Frase feita com amor</h5>
+              <p className="font-normal text-xs">{flowers?.phrase}</p>
+            </div>
+          </main>
+          <footer className="w-full bg-black bg-opacity-50 rounded p-2 text-white flex flex-col justify-center items-center gap-1 font-medium text-xs">
+            <p>De: Maclean ⚡</p>
+            <p>Para: A mulher da minha vida 👰💒</p>
+          </footer>
+        </div>
       </section>
     </>
   )
